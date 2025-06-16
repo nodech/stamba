37d270a7556957d129fa0a5d9b2183a2d4fa5889
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2025-06-16 22:48:30 +0400

    main: add default page options.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 use std::io;
+use clap::{Parser, ValueEnum};
 
 mod page;
 mod app;
 
+#[derive(Debug, ValueEnum, Clone)]
+enum LoadablePages {
+    QuickGame,
+    Home,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Show frame count
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Default page to load
+    #[arg(short, long, default_value_t = LoadablePages::Home,
+           value_enum, help = "The page to load on startup")]
+    page: LoadablePages,
+}
+
 fn main() -> io::Result<()> {
-    let debug = std::env::var("DEBUG")
-        .is_ok_and(|v| v == "1" || v.to_lowercase() == "true");
+    let args = Args::parse();
+
+    let mut pages: Vec<Box<dyn page::Page>> = vec![
+        Box::new(page::MenuPage::default())
+    ];
+
+    if let LoadablePages::QuickGame = args.page {
+        pages.push(Box::new(page::GamePage::new()));
+    }
 
     let mut app = app::App {
-        debug,
+        debug: args.debug,
+        pages,
         ..Default::default()
     };
 
